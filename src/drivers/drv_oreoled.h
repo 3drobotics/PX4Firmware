5a57c0b97d54d498727feb5356d556f01cf9dcea
Will Silva <will.silva@3drobotics.com>
2016-06-17 08:56:00 -0700

    oreoled: remove bootloader specific code

--- src/drivers/drv_oreoled.h
+++ src/drivers/drv_oreoled.h
@@ -64,29 +64,8 @@
 /** send reset */
 #define OREOLED_SEND_RESET		_OREOLEDIOC(4)
 
-/** boot ping */
-#define OREOLED_BL_PING			_OREOLEDIOC(5)
-
-/** boot version */
-#define OREOLED_BL_VER			_OREOLEDIOC(6)
-
-/** boot write flash */
-#define OREOLED_BL_FLASH		_OREOLEDIOC(7)
-
-/** boot application version */
-#define OREOLED_BL_APP_VER		_OREOLEDIOC(8)
-
-/** boot application crc */
-#define OREOLED_BL_APP_CRC		_OREOLEDIOC(9)
-
-/** boot startup colour */
-#define OREOLED_BL_SET_COLOUR	_OREOLEDIOC(10)
-
-/** boot application */
-#define OREOLED_BL_BOOT_APP		_OREOLEDIOC(11)
-
 /** force an i2c gencall */
-#define OREOLED_FORCE_SYNC		_OREOLEDIOC(12)
+#define OREOLED_FORCE_SYNC		_OREOLEDIOC(5)
 
 /* Oreo LED driver supports up to 4 leds */
 #define OREOLED_NUM_LEDS		4
@@ -102,39 +81,6 @@
 
 /* maximum number of commands retries */
 #define OEROLED_COMMAND_RETRIES	2
-#define OEROLED_BOOT_COMMAND_RETRIES 10
-
-/* magic number used to verify the software reset is valid */
-#define OEROLED_RESET_NONCE				0x2A
-
-/* microseconds to hold-off between write and reads */
-#define OREOLED_WRITE_READ_HOLDOFF_US	500
-
-/* microseconds to hold-off between retries */
-#define OREOLED_RETRY_HOLDOFF_US		200
-
-#define OREOLED_BOOT_FLASH_WAITMS		10
-
-#define OREOLED_BOOT_SUPPORTED_VER		0x01
-
-#define OREOLED_BOOT_CMD_PING			0x40
-#define OREOLED_BOOT_CMD_BL_VER			0x41
-#define OREOLED_BOOT_CMD_APP_VER		0x42
-#define OREOLED_BOOT_CMD_APP_CRC		0x43
-#define OREOLED_BOOT_CMD_SET_COLOUR		0x44
-
-#define OREOLED_BOOT_CMD_WRITE_FLASH_A	0x50
-#define OREOLED_BOOT_CMD_WRITE_FLASH_B	0x51
-#define OREOLED_BOOT_CMD_FINALISE_FLASH	0x55
-
-#define OREOLED_BOOT_CMD_BOOT_APP		0x60
-
-#define OREOLED_BOOT_CMD_PING_NONCE		0x2A
-#define OREOLED_BOOT_CMD_BOOT_NONCE		0xA2
-
-#define OREOLED_FW_FILE_HEADER_LENGTH	2
-#define OREOLED_FW_FILE_SIZE_LIMIT		6144
-#define OREOLED_FW_FILE					"/etc/firmware/oreoled.bin"
 
 /* enum passed to OREOLED_SET_MODE ioctl()
  *	defined by hardware */
